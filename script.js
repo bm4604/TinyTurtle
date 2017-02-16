350e38c4095247b8fc8b658c70fd9ac805ef3b35
Brandon Mattis <bmattis2018@bklawtech.com>
2017-02-16 20:56:10 +0000

    changes

---
+++ script.js
@@ -0,0 +1,13 @@
+ TinyTurtle.apply(window);
+ 
+ /* global TinyTurtle */
+ 
+ forward:(50);
+ right:(90);
+ forward:(50);
+ right:(90);
+ forward:(50);
+ right:(90);
+ forward:(50);
+ right:(90);
+ stamp();
